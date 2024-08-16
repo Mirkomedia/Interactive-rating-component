9427bc217abcb664fe1ae97fa3aef07340c1305c
Mirkomedia <mirko.weibel18@gmail.com>
2024-08-16 13:10:13 +0300

    +

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const form = document.querySelector("#rating");
+const form = document.querySelector('.rating');
+const thankYou = document.querySelector('.thank-you')
+form.addEventListener("submit", changeState)
+function changeState () {
+ 
+    thankYou.removeAttribute("hidden")
+    console.log('removed')
+    } 
 
-async function sendData() {
-    
-}
+
+
+
