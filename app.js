869b00119b7235dd030a7f70a1807e94ed612408
Mirkomedia <mirko.weibel18@gmail.com>
2024-08-19 12:18:38 +0300

    +

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
-const form = document.querySelector('.rating');
-const thankYou = document.querySelector('.thank-you')
-form.addEventListener("submit", changeState)
-function changeState () {
- 
-    thankYou.removeAttribute("hidden")
-    console.log('removed')
-    } 
+var thanks = document.querySelector(".thank-you")
+var hwdw = document.getElementById("hwdw")
+const ratingBtn = document.querySelectorAll(".round-btn")
+const ratingDisplay = document.querySelector('.rating-display')
+const submitBtn = document.querySelector('.submit-btn')
 
 
 
+   ratingBtn.forEach(button => {
+
+    button.addEventListener("click", () =>{
+
+    ratingBtn.forEach(button => button.classList.remove('active'))
+
+    button.classList.add('active')
+
+   const ratingValue = button.getAttribute('data-value')
+
+    ratingDisplay.textContent = ratingValue
+    })
+
+
+});
+if(submitBtn ){
+submitBtn.addEventListener('click', ()=>{
+    if(thanks){
+        thanks.removeAttribute('hidden')
+    }}
+)}
+
+
+
+
+
+    
 
